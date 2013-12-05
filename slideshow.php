cfe1433ad91beec8b6eef711a5b9eae5e6094557
liquidthex <github.liquid-x@liquid-x.net>
2013-12-04 19:42:19 -0800

    first working added config variable gallery_url

--- slideshow.php
+++ slideshow.php
@@ -3,3 +3,25 @@
 /*************
  * Slideshow app
  *************/
+
+include_once("lib/NexusGallery.php");
+
+$ng = new NexusGallery();
+$url = $ng->getConfig('gallery_url');
+
+$img = $ng->nextImage();
+$imgname = basename($img);
+$imgurl = $url . '/' . $img;
+
+echo "<html><head><title>Nexus Gallery</title>";
+echo "<style>
+html,body {
+ margin: 0;
+ padding: 0;
+ background: black;
+ text-color: white;
+}
+</style>";
+echo "</head><body>";
+echo "<img src=\"" . $imgurl . "\">";
+echo "</body></html>";

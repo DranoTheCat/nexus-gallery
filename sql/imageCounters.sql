6cea3bfc2e30e9c9e84441d03c3ffd4dff78ed79
Dan Hart <dan@danhartfiction.com>
2013-12-07 08:40:50 -0800

    Better SQL import files

---
+++ sql/imageCounters.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 07, 2013 at 04:39 PM
+-- Server version: 5.5.33a-MariaDB
+-- PHP Version: 5.4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `prototype`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `imageCounters`
+--
+
+CREATE TABLE IF NOT EXISTS `imageCounters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filepath` mediumtext NOT NULL,
+  `thumbs_up` int(11) NOT NULL,
+  `thumbs_down` int(11) NOT NULL,
+  `num_views` int(11) NOT NULL,
+  `last_view` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=74 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

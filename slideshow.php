2aa6a784f5cccb3b64d2f2c30f9b1c2a9ee4a2a3
liquidthex <github.liquid-x@liquid-x.net>
2013-12-08 10:51:19 -0800

    Responsive Resizing

--- slideshow.php
+++ slideshow.php
@@ -16,17 +16,42 @@ html,body {
  margin: 0;
  padding: 0;
  background: black;
- text-color: white;
+ color: white;
 }
 </style>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" ></script>
 <script type="text/javascript">
+function scaleSize(currW, currH){
+ var maxW = $(document).width();
+ var maxH = $(document).height();
+ var ratio = currH / currW;
+ if (ratio > 1){
+  if (currH > maxH) {
+   currH = maxH;
+   currW = currH / ratio;
+  } else if (currW > maxW) {
+   currW = maxW;
+   currH = currW * ratio;
+  }
+ } else {
+  if (currW > maxW) {
+   currW = maxW;
+   currH = currW * ratio;
+  } else if (currH > maxH) {
+   currH = maxH;
+   currW = currH / ratio;
+  }
+ }
+ return [currW, currH];
+}
 $(function() {
  slideshowAjax();
+ //testLoop();
 });
 $.ajaxSetup({ cache: false });
 var ajaxInAction = false;
 var url = '<?echo $url;?>';
+var myImg = null;
 function slideshowAjax() {
  if (ajaxInAction) { return; }
  ajaxInAction = true;
@@ -36,15 +61,38 @@ function slideshowAjax() {
    var jsonData = eval('(' + result + ')');
    var curImg = url + '/' + jsonData[0];
    var timeLeft = jsonData[1] * 1000;
-   document.getElementById('image').src = curImg;
+   myImg = $('<img id="dynamic">');
+
+//myImg.on('load', function(){
+   myImg.load(function() {
+    var newSize = scaleSize(this.width, this.height);
+
+    $('#image').replaceWith(this);
+
+    document.getElementById('image').width = newSize[0];
+    document.getElementById('image').height = newSize[1];
+   });
+
+   myImg.attr('id', 'image');
+   myImg.attr('src', curImg);
+
    setTimeout('slideshowAjax();', timeLeft);
    ajaxInAction = false;
   }
  });
+}
+
+function testLoop() {
+   var newSize = scaleSize(document.getElementById('image').width, document.getElementById('image').height);
+ document.getElementById('debugtext').innerHTML = 'W: ' + document.getElementById('image').width + ' ' + 'H: ' + document.getElementById('image').height + ' ' + 'NW: ' + newSize[0] + ' ' + 'NH: ' + newSize[1];
+//   document.getElementById('image').width = newSize[0];
+//   document.getElementById('image').height = newSize[1];
+ setTimeout('testLoop()', 500);
 }
 </script>
 </head><body>
 <?
 
 echo "<img src=\"loading.gif\" id=image>";
+#echo "<div id=imagediv></div>";
 echo "</body></html>";
